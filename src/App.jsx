917b63523d609c559073add711db9f6b05e0d2f8
IuryFredson <iuryfredsongm@gmail.com>
2025-03-17 20:04:19 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -225,7 +225,7 @@ function App() {
               <div className="h-48 bg-gray-700 relative">
                 {/* Imagem da MedFácil */}
                 <img
-                  src="/medfacil.png"
+                  src="/MEDFACIL.png"
                   alt="Clínica MedFácil"
                   className="absolute inset-0 w-full h-full object-cover"
                 />
@@ -255,7 +255,7 @@ function App() {
               <div className="h-48 bg-gray-700 relative">
                 {/* Imagem da Central Clinic */}
                 <img
-                  src="/centralclinic.jpg"
+                  src="/CENTRALCLINIC.jpg"
                   alt="Central Clinic"
                   className="absolute inset-0 w-full h-full object-cover"
                 />
@@ -284,7 +284,7 @@ function App() {
               <div className="h-48 bg-gray-700 relative">
                 {/* Imagem da Quallivida */}
                 <img
-                  src="/qualivida.jpg"
+                  src="/QUALIVIDA.jpg"
                   alt="Clínica Quallivida"
                   className="absolute inset-0 w-full h-full object-cover"
                 />
